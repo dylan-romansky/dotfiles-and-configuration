1aa04cab0672c5a89ef1f10d6c7c3eb5deb0a84d
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-03-22 15:50:24 -0700

    switched my laptop back over to NetworkManager as its autoretry is better than the one in my scripts and I really need that right now due to a failing network card. chrome-start has also been updated with the ability to take preferred capabilities to further enhance the automating experience

--- python_user_modules/chrome_start.py
+++ python_user_modules/chrome_start.py
@@ -5,9 +5,9 @@ from selenium.webdriver.chrome.options import Options
 
 chromedriver = '/home/dromansk/builds/chromedriver/pkg/chromedriver/usr/bin/chromedriver'
 
-def chrome_start():
+def chrome_start(capabilities=None):
 	options = Options()
 	options.add_experimental_option("excludeSwitches", ['enable-automation'])
 	options.add_experimental_option("useAutomationExtension", [False])
 	options.add_argument('--user-data-dir=/home/dromansk/.config/google-chrome/')
-	return webdriver.Chrome(chromedriver, options=options)
+	return webdriver.Chrome(chromedriver, options=options, desired_capabilities=capabilities) 
