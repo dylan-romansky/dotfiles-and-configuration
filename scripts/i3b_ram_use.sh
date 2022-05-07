8c49423b254e2f514b84ff7ae6dfb44b59f71089
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-05-06 21:21:24 -0700

    starting to get into using rofi. also fixed an issue where I forgot that linux technically keeps a lot of things loaded in memory even if it's not actively being used and can be dumped at the drop of a hat. that was causing my ram monitoring i3blocks script to choose the incorrect colour, as                total        used        free      shared  buff/cache   available Mem:        16235976     1800020     7430332      209292     7005624    13888120 Swap:              0           0           0's free field is exclusively how much ram is entirely unused. Did some math to fix that

--- scripts/i3b_ram_use.sh
+++ scripts/i3b_ram_use.sh
@@ -9,12 +9,11 @@
 
 TOTAL=0
 USED=1
-FREE=2
 RAM=($(free | awk 'FNR==2 { print $2 " " $3 " " $4 }'))
 OUT="$(bc <<< "${RAM[$USED]} / 1048576")GB/$(bc <<< "${RAM[$TOTAL]} / 1048576")GB"
 echo -e "<span font=\"Font Awesome 6 Free\">\xef\x83\xa8</span>: $OUT"
 echo -e "$OUT"
-FREE=$(bc <<< "${RAM[$FREE]} / 1048576")
+FREE=$(bc <<< "(${RAM[$TOTAL]} - ${RAM[$USED]}) / 1048576")
 if [ $FREE -le 2 ]; then
 	echo "#FF0000"
 elif [ $FREE -le 7 ]; then
