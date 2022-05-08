5056ea8765080f8f13e0c09b484b16cf3e078082
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-05-08 12:28:04 -0700

    fixed sizable bug in i3config and added bash function to kill all instances of all given programs

--- scripts/i3b_vol_mic.sh
+++ scripts/i3b_vol_mic.sh
@@ -3,28 +3,21 @@
 #simple script for i3blocks that gets the volume or mic
 #levels and prints them with a unicode icon
 
-while [ -z $LEV ]; do
-	if [ -z $1 ]; then
-		LEV=$(pactl get-sink-volume @DEFAULT_SINK@ | awk 'FNR==1{ print " " $5 }')
-	else
-		LEV=$(pactl get-source-volume @DEFAULT_SOURCE@ | awk 'FNR==1{ print " " $5 }')
-	fi
+while [ -z $SLEV ] && [ -z $MLEV ]; do
+	SLEV=$(pactl get-sink-volume @DEFAULT_SINK@ | awk 'FNR==1{ print " " $5 }')
+	MLEV=$(pactl get-source-volume @DEFAULT_SOURCE@ | awk 'FNR==1{ print " " $5 }')
 done
-if [ -z $1 ]; then
-	if [[ "$(pactl get-sink-mute @DEFAULT_SINK@)" == *"yes"* ]]; then
-		STAT="\xef\x9a\xa9:"
-	else
-		DEC=$(echo $LEV | cut -d% -f1)
-		if [ $DEC -le 20 ]; then
-			STAT="\xef\x80\xa6"
-		elif [ $DEC -le 60 ]; then
-			STAT="\xef\x80\xa7"
-		else
-			STAT="\xef\x80\xa8"
-		fi
-	fi
+if [[ "$(pactl get-sink-mute @DEFAULT_SINK@)" == *"yes"* ]]; then
+	SSTAT="\xef\x9a\xa9:"
 else
-	STAT="\xef\x84\xb0"
+	DEC=$(echo $SLEV | cut -d% -f1)
+	if [ $DEC -le 20 ]; then
+		SSTAT="\xef\x80\xa6"
+	elif [ $DEC -le 60 ]; then
+		SSTAT="\xef\x80\xa7"
+	else
+		SSTAT="\xef\x80\xa8"
+	fi
 fi
-echo -e "<span font=\"Font Awesome 6 Free\">$STAT</span>$LEV"
-echo -e "<span font=\"Font Awesome 6 Free\">$STAT</span>$LEV"
+MSTAT="\xef\x84\xb0"
+echo -e "<span font=\"Font Awesome 6 Free\">$SSTAT</span>$SLEV <span font=\"Font Awesome 6 Free\">$MSTAT</span>$MLEV"
