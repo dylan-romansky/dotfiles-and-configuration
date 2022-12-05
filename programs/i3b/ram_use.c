07d1f6f6a36e3f00bc58c31892e6dc362e66c83f
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-12-04 17:11:17 -0800

    miscalculated bufsize

--- programs/i3b/ram_use.c
+++ programs/i3b/ram_use.c
@@ -2,20 +2,25 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
+#define BUFSIZE 84
+#define LINESIZE 28
 
+//MemTotal:       16233788 kB
+//MemFree:         6667884 kB
+//MemAvailable:   10139944 kB
 int main()	{
-	char buf[38];
+	char buf[BUFSIZE];
 	int fd = open("/proc/meminfo", O_RDONLY);
 	if (fd < 0)	{
 		printf("Error: couldn't open file\n");
 		return 1;
 	}
-	if (read(fd, &buf, 37) < 0)	{
+	if (read(fd, &buf, BUFSIZE) < 0)	{
 		printf("Error: couldn't read file\n");
 		return 2;
 	}
 	int total = atoi(buf + 9) / 1000000;
-	int free = atoi(buf + 27) / 1000000;
+	int free = atoi(buf + LINESIZE + 10) / 1000000;
 	int quart = total / 4;
 	char *ico = "\xef\x94\xb8";
 	char *col = "#FF0000";
